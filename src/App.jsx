dcc906181030f918379ce653f9bb15b94421f4e6
shivajreddy <shivajayaramireddy@gmail.com>
2022-06-30 12:47:26 -0400

    custom fonts via css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Cooking App</h1>
+      <h1 style={{ textAlign: 'center' }}>Cooking App</h1>
+
+      <h2 style={{ fontWeight: "bold" }}> Bold </h2>
+      <h2 style={{ fontWeight: "normal" }}> Normal </h2>
+      <h2 style={{ fontWeight: "lighter" }}> Lighter</h2>
     </div>
   );
 }
